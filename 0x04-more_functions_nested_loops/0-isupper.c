0f07f2c8a0b4a5097ab4fa88b2e177e5068425a8
JEPTUI <alicejeptui@gmail.com>
2022-12-15 06:02:56 -0800

    Check for uppercase character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
  * _isupper - checks for uppercase character
  * @c: character to be checked
  *
  * Return: 1 if uppercase 0 if false
  */
+
 int _isupper(int c)
 {
 	if ((c >= 'A') && (c >= 'Z'))
