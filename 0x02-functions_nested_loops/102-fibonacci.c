685d33612766d66aacde381dc31b08083840a6df
JEPTUI <alicejeptui@gmail.com>
2022-12-14 20:19:00 -0800

    Fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,19 +5,21 @@
  */
 int main(void)
 {
-	long int num1 = 1;
-	long int num2 = 2;
-	int fibn;
+	long int num1;
+	long int num2;
+	long int fibn;
 	int counter;
 
+	num1 = 1;
+	num2 = 2;
 	printf("%ld, %ld", num1, num2);
 
 	for (counter = 0; counter < 48; counter++)
 	{
 		fibn = num1 + num2;
 		printf(", %ld", fibn);
-		num1 += num2;
-		num2 += fibn
+		num1 = num2;
+		num2 = fibn
 	}
 	printf("\n");
 
