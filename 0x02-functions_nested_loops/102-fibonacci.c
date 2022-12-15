59637d91586a3855ad3ca52a456b950a438a1608
JEPTUI <alicejeptui@gmail.com>
2022-12-14 19:31:27 -0800

    Fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main -prints the first 50 Fibonacci numbers
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long int num1;
+	long int num2;
+	long int fibn;
+	int counter;
+
+	num1 = 1;
+	num2 = 2;
+	printf("%ld, %ld", num1, num2);
+	for (counter = 1; counter < 50; counter++)
+	{
+		fibn = num1 + num2;
+		printf(", %ld", fibn);
+		num1 = num2;
+		fibn = num2;
+	}
+	printf("\n");
+
+	return (0);
+}
