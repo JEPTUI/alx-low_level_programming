161b55b1bf673f925a7954d742740322c2fe6f7c
JEPTUI <alicejeptui@gmail.com>
2022-12-14 21:58:38 -0800

    Find and print fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - prints the first 98 Fibonacci numbers
+ * Return: Always 0.
+ */
+int main(void)
+{
+	unsigned long int num1, num2, sum;
+	int counter;
+
+	num1 = 1;
+	num2 = 2;
+	sum = 0;
+
+	for (counter = 1; counter <= 98; counter++)
+	{
+		sum = num1 + num2;
+		if (num1 != 98)
+		{
+			printf("%lu, ", sum);
+		}
+		else
+		{
+			printf("%lu\n", sum);
+		}
+		num1 = num2;
+		num2 = sum;
+	}
+	return (0);
+}
