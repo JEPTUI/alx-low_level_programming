923702de51d898746978160371e6bf483d5db258
JEPTUI <alicejeptui@gmail.com>
2022-12-16 01:36:15 -0800

    Fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,13 +15,13 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
-		else if (i % 3 == 0)
-			printf("Fizz");
 		else if (i % 5 == 0)
 			printf("Buzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
 		else
 			printf("%d", i);
-		
+
 		printf(" ");
 	}
 	printf("\n");
