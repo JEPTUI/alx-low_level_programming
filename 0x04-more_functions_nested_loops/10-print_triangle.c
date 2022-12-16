e6438df03f8c15b78be8ad7e477597f23492986b
JEPTUI <alicejeptui@gmail.com>
2022-12-16 01:10:21 -0800

    Draw a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,8 +11,8 @@ void print_triangle(int size)
 
 	if (size <= 0)
 		_putchar('\n');
-
-	for (height = 1; height <= size; height++)
+	else
+		for (height = 1; height <= size; height++)
 		{
 			for (width = 1; width <= (size - height); width++)
 				_putchar(' ');
