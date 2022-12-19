a5491959abe61b97f7209957ad535403aa3f5e19
JEPTUI <alicejeptui@gmail.com>
2022-12-19 09:17:35 -0800

    Convert string to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _atoi - converts string to an integer
+ * @s: string
+ *
+ * Return: on success the integer value of converted string
+ * 0 if there are no numbers in the string
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
+
+	return (num * sign);
+}
