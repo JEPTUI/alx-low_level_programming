8e37ac96a77e340d646b479077ab555b8d818506
JEPTUI <alicejeptui@gmail.com>
2022-12-19 09:30:19 -0800

    Convert string to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,8 +14,10 @@ int _atoi(char *s)
 	do {
 		if (*s == '-')
 			sign *= -1;
-		else if (*s >= '0' && s <= '9')
+
+		else if (*s >= '0' && *s <= '9')
 			num = (num * 10) + (*s - '0');
+
 		else if (num > 0)
 			break;
 	} while (*s++);
