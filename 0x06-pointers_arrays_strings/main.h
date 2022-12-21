306ae27667ffcc07f4cec32edb00c372a1e39edb
JEPTUI <alicejeptui@gmail.com>
2022-12-21 03:31:55 -0800

    Print an integer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,5 +11,7 @@ char *string_toupper(char *str);
 char *cap_string(char *str);
 char *leet(char *s);
 char *rot13(char *str);
+int _putchar(char c);
+void print_number(int n);
 
 #endif /* MAIN_H */
