2f06b0395a3600b3ccf87f3a2cea971744b6063a
JEPTUI <alicejeptui@gmail.com>
2023-01-17 05:23:05 -0800

    Hot fix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 /**
  * init_dog - initialize a variable of type struct dog
  * @d: a variable of type struct dog
@@ -8,6 +9,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
