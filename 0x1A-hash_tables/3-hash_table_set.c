03339b9e9e6479e5bd65d33a844c4574f7fffcce
JEPTUI <alicejeptui@gmail.com>
2023-04-06 05:21:57 -0700

    Update

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node;
 	char *new_value;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+	if (!ht || !key || !value)
 		return (0);
 
 	new_value = strdup(value);
