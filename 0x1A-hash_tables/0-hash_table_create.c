e7339b75f4b9e4478b09e68e4d2947d9fc3f0550
JEPTUI <alicejeptui@gmail.com>
2023-04-07 03:05:47 -0700

    Hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,25 +9,20 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
 	unsigned long int i = 0;
-	hash_node_t **array;
 
-	if (size == 0)
-		return (NULL);
 	ht = malloc(sizeof(hash_table_t));
 	if (!ht)
 		return (NULL);
 
-	array = malloc(sizeof(hash_node_t *) * size);
-	if (!array)
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (!ht->array)
 	{
 		free(ht);
 		return (NULL);
 	}
 	for (i = 0; i < size; i++)
-		array[i] = NULL;
-
-	ht->size = size;
-	ht->array = array;
+		ht->array[i] = NULL;
 
 	return (ht);
 }
